eb899d1eb5eef339ab9c85a56daef79bdadf7b84
tindoann <tindoann@gamil.com>
2019-12-01 13:04:00 -0500

    completed todo render list

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
   }; 
 
   const addGoalHandler = () => {
-    setEnteredGoal(enteredGoal); 
+    setEnteredGoal(currentGoals => [...currentGoals, { key: Math.random().toString(), value: enteredGoal}]); 
   }; 
 
   const addGoalHandler = () => {
@@ -28,25 +28,41 @@ export default function App() {
       />
       <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
       </View>
-    </View>
-  );
-}
+      );        
+    }
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50
   }, 
   inputContainer: {
-  container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
- }
-});
+  input: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  }, 
+  listItem: {
+    padding: 10, 
+    backgroundColor: '#ccc', 
+    borderColor: 'black', 
+    borderWidth: 1
+  }
+ });
 
 const styles = StyleSheet.create({})
 
