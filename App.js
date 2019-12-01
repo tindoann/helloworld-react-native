86dd8816c1cc4d315631cfff5e26884ed0be9bd6
tindoann <tindoann@gamil.com>
2019-11-30 19:52:03 -0500

    setup andriod app template

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Course Goal"
+        style={{ 
+          width: '80%', 
+          borderColor: 'black', 
+          borderWidth: 1, 
+          padding: 10
+        }}
+      />
+      <Button title="ADD" />
+      </View>
+      <View />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }, 
+  inputContainer: {
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+ }
 });
+
+const styles = StyleSheet.create({})
+
+
+
