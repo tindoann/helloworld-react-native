c19106d589901394e8ba75e52708c7bf59c05d22
tindoann <tindoann@gamil.com>
2019-12-01 22:25:41 -0500

    added delete option

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,11 +3,19 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const GoalItem = props => {
   return (
-    <View style={style.listItem}>
-      <Text>{itemData.item.value}</Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete} >
+      <View style={style.listItem}>
+        <Text>{itemData.item.value}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }; 
+
+const removeGoalHandler = goalId => {
+  setCourseGoals(currentGoals => {
+    return currentGoals.filter((goal) => goal.id !== goalId); 
+  }); 
+}
 
 const styles = StylesSheet.create({
   listItem: {
